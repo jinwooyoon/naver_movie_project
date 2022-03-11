71c07b70a9443223c44a1a527094a965f45ec883
jinwoo <say_stop@naver.com>
2022-03-11 17:24:37 +0900

    'third'

--- ML3.py
+++ ML3.py
@@ -3,20 +3,63 @@ import pandas as pd
 from scipy import sparse
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
+from collections import Counter
 
-
-# %%
-data = pd.read_csv('./real_data22222.csv',encoding='cp949')
 
 #%%
 
+choice_filtering = [['신세계','애니메이션'],['알라딘','범죄'],['다크나이트','범죄']]
+
+genre_filter = []
+
+for genre in choice_filtering:
+    genre_filter.append(genre[1])
+
+a = Counter(genre_filter)
+
+print(a)
+
+
+user_movie = []
+
+
+#%%
+for i,item in enumerate(choice_filtering):
+    if choice_filtering[0][1] == item[1]:
+        user_movie.append(item)
+
+
+
+
+#%%
+
+user_movie
+
+ # %%
+data = pd.read_csv('./final_data.csv',encoding='cp949')
+
+#%%
+
+data = data[['movie_id','user_id','movie_title','rating','genre']]
+
+
+
+
+
+#%%
+
+
+# data_genre = (data.genre =='드라마') | (data.genre =='애니메이션')
+
+
+#%%
+
+data_genre = data['genre'] =='범죄'
+data = data[data_genre]
 data
 
-#%%
-
-len(data['user_name'].unique())
 # %%
-user_score = data.pivot_table(index=['user_id'],columns=['movie_title'],values='reple_score')
+user_score = data.pivot_table(index=['user_id'],columns=['movie_title'],values='rating')
 # %%
 user_score.head()
 # %%
@@ -26,6 +69,7 @@ user_scores.head(10)
 # %%
 course_similarity_df = user_scores.corr(method='pearson')
 course_similarity_df.head(10)
+
 # %%
 def get_similar_courses(course_name,user_score):
     similar_score = course_similarity_df[course_name] * (user_score - 2.5)
@@ -33,15 +77,16 @@ def get_similar_courses(course_name,user_score):
     
     return similar_score
 
-print(get_similar_courses('127 시간',9))
-# %%
-haha = [("범죄도시",10),("신세계",10),("살인의 추억",10)]
+# print(get_similar_courses('127 시간',9))/
+
 similar_scores = pd.DataFrame()
 
-for course,score in haha:
+
+user_choice = [("신세계",10),("범죄와의 전쟁 : 나쁜놈들 전성시대",10),("대부",10)]
+for course,score in user_choice:
     similar_scores = similar_scores.append(get_similar_courses(course,score),ignore_index=True)
     
 similar_scores.head()
 # %%
-similar_scores.sum().sort_values(ascending=False)[:10]
+similar_scores.sum().sort_values(ascending=False).head(10)
 # %%
--- data_repair.py
+++ data_repair.py
@@ -3,49 +3,107 @@ import pandas as pd
 
 
 #%%
-data = pd.read_csv('./data_result.csv',encoding='utf-8')
+data = pd.read_csv('important_data.csv',encoding='cp949')
+
+#%%
+
+data
+#%%
+
+data2 = pd.read_csv('important_data.csv',encoding='cp949')
+
+data2
+
+
+# %%
+data.drop('user_id',axis=1,inplace=True)
+# %%
+
+data_name = list(data['user_name'].unique())
+
+#%%
+
+
+for i,name in enumerate(data_name):
+    data[data['user_name'] == f'{name}'] = i+1
 
 
 #%%
 
-data2  = pd.read_csv('./dabinnim.csv',encoding='cp949')
+user_name = data['user_name']
+#%%
+
+data2.drop('user_name',axis=1,inplace=True)
+#%%
+
+data2['user_id'] = user_name
+#%%
+
+data2.rename(columns={'id':'movie_id','title':'movie_title'},inplace=True)
+#%%
+
 data2
 #%%
-data2 = data2['title']
+
+data2 = data2[['user_id','user_name','movie_id','movie_title','reple_score']]
+#%%
+data2
+#%%
+
+data2.to_csv('./real_data22222.csv',encoding='cp949',index=False)
+#%%
+data_name.to_csv('./name_list.csv',encoding='cp949')
 # %%
-data_list = data['user_name'].unique()
+data[data['user_name']=='꾸믈꾸어(yoon****)'] = 1
+
 # %%
-data_list = list(data_list)
+data['user_name'].value_counts()
+# %%
+
+import pandas as pd
+
+data1 = pd.read_csv('./real_data22222.csv',encoding='cp949')
+
+
+# %%
+data2 = pd.read_csv('project_data/result_data.csv',encoding='utf-8')
+# %%
+data1
+# %%
+data2
+# %%
+
+data1 = data1.values.tolist()
+data2 = data2.values.tolist()
+#%%
+data2
+
+#%%
+new_data = []
+
+for i in data1:
+    for j in data2:
+        if i[0] == j[0]:
+            
+
+# %%
+
+for i,item in enumerate(data1):
+    for j in data2:
+        if item[0] ==j[0]:
+            for k in j:
+                data1[i].append(k)
 
 #%%
 
-data_list
-
-#%%
-
-data['title'] = data2
-#%%
-
-data
-#%%
-
-data_add = []
-for i in range(len(data['user_name'])):
-    data_add.append(i+1)
-    
-    
-
 
 # %%
 
-data['user_id'] = data_add
+result = pd.DataFrame(data1)
+
 #%%
 
-data
-# %%
-data.drop('user_name',axis=1,inplace=True)
-# %%
-data = data[['user_id','id','title','reple_score']]
-# %%
-data.to_csv('./test_data.csv',encoding='utf-8',index=None)
+res
+#%%
+result.to_csv('./real_dataee321421e.csv',encoding='utf-8')
 # %%
